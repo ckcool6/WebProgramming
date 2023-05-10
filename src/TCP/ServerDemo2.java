1fe374e97a992e9d5591a0e751205b1a246a5047
rzdtjb <rzdtjb@gmail.com>
2023-05-10 19:47:57 +0800

    Client Server TCP

--- src/TCP/ServerDemo2.java
+++ src/TCP/ServerDemo2.java
@@ -3,27 +3,29 @@ package TCP;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.UUID;
 
 public class ServerDemo2 {
     public static void main(String[] args) throws IOException {
         ServerSocket ss = new ServerSocket(10001);
 
-        Socket accept = ss.accept();
+        while (true) {
+            Socket accept = ss.accept();
 
-        BufferedInputStream bis = new BufferedInputStream(accept.getInputStream());
-        BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream("copy.jpg"));
+            BufferedInputStream bis = new BufferedInputStream(accept.getInputStream());
+            BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(UUID.randomUUID().toString() + ".jpg"));
 
-        int b;
-        while ((b = bis.read())!=-1){
-            bos.write(b);
+            int b;
+            while ((b = bis.read()) != -1) {
+                bos.write(b);
+            }
+            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(accept.getOutputStream()));
+            bw.write("上传成功");
+            bw.newLine();
+            bw.flush();
+
+            bos.close();
+            accept.close();
         }
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(accept.getOutputStream()));
-        bw.write("上传成功");
-        bw.newLine();
-        bw.flush();
-
-        bos.close();
-        accept.close();
-        ss.close();
     }
 }
