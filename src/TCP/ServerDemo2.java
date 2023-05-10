3881191b83460d2dcd18fa0da161f2954b3b3fd2
rzdtjb <rzdtjb@gmail.com>
2023-05-10 20:06:30 +0800

    ThreadPool socket

--- src/TCP/ServerDemo2.java
+++ src/TCP/ServerDemo2.java
@@ -4,15 +4,27 @@ import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.UUID;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 public class ServerDemo2 {
     public static void main(String[] args) throws IOException {
         ServerSocket ss = new ServerSocket(10001);
-
+        ThreadPoolExecutor pool = new ThreadPoolExecutor(
+                3,
+                10,
+                60,
+                TimeUnit.SECONDS,
+                new ArrayBlockingQueue<>(5),
+                Executors.defaultThreadFactory(),
+                new ThreadPoolExecutor.AbortPolicy()
+        );
         while (true) {
             Socket accept = ss.accept();
             ThreadSocket ts = new ThreadSocket(accept);
-            new Thread(ts).start();
+            pool.submit(ts);
         }
     }
 }
